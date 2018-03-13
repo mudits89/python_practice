d97534b460826ad21e5a42ae6673e75f0a135153
mudits89 <mudits_89@hotmail.com>
2018-03-13 20:39:12 +0800

    Add files via upload

---
+++ python_prac6(pandas).py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 19:53:17 2018
+
+@author: mudit
+"""
+
+import numpy as np
+import pandas as pd
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+d = pd.DataFrame(exam_data, index = labels)
+print("Original Rows:")
+print(d)
+print("\n Append a new row:")
+d.loc['k'] = [1, 'Suresh', 'yes', 15.5] 
+print("Print all records after insert a new record:")
+print(d)
+print("\n Delete new row and display original rows:")
+d = d.drop('k')
+print(d)
+
+
+import numpy as np
+import pandas as pd
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+d = pd.DataFrame(exam_data, index = labels)
+print("Original Rows:")
+print(d)
+d.sort_values(by=['name','score'], ascending = [False, True])
+print("Sort the dataframe first by 'name' in descending order then by 'score' in ascending order:")
+print(d)
+
+
+import numpy as np
+import pandas as pd
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+d = pd.DataFrame(exam_data, index = labels)
+print("Original Rows:")
+print(d)
+print("\n Replace the 'qualify' column value 'yes' and 'no' with True and False:")
+d['qualify'] = d['qualify'].map({'yes':True,'no':False})
+print(d)
+
+
+import numpy as np
+import pandas as pd
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+d = pd.DataFrame(exam_data, index = labels)
+print("Original Rows:")
+print(d)
+print("\n Change the names 'James' to 'Suresh':")
+d['name'] = d['name'].replace('James','Suresh')
+print(d)
