421ce23190171beec6cce830c7d6aed5abf7b8e3
mudits89 <mudits_89@hotmail.com>
2018-03-16 09:05:51 +0800

    Add files via upload

---
+++ python_prac1(100pandaspuzzles).py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 08:35:52 2018
+
+@author: mudit
+"""
+
+import pandas as pd
+print(pd.__version__)
+
+import pandas as pd
+pd.show_versions()
+
+import numpy as np
+import pandas as pd
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+d = pd.DataFrame(data, index = labels)
+print(d)
+
+
+import numpy as np
+import pandas as pd
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+d = pd.DataFrame(data, index = labels)
+print("Summary of the basic information about this DataFrame and its data:")
+print(d.info())
+
+
+import numpy as np
+import pandas as pd
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+d = pd.DataFrame(data, index=labels)
+print("First three rows of the Data Frame:")
+print(d.iloc[:3])
